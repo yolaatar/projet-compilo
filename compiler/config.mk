1873b0d6fb6a41cdcfda556850539d93b1301134
Nabil Dakkoune <nabil.dakkoune.86@hotmail.com>
2025-04-01 09:06:15 +0200

    Update .gitignore and clean repository

--- compiler/config.mk
+++
@@ -1,5 +0,0 @@
-# the ANTLRJAR part below is copied from /usr/bin/antlr4
-ANTLRJAR=/usr/share/java/stringtemplate4.jar:/usr/share/java/antlr4.jar:/usr/share/java/antlr4-runtime.jar:/usr/share/java/antlr3-runtime.jar/:/usr/share/java/treelayout.jar
-ANTLRINC=/usr/local/include/antlr4-runtime/
-ANTLRLIB=/usr/local/lib/libantlr4-runtime.a
-ANTLR=java -jar ~/antlr4-install/antlr-4.13.2-complete.jar
